b839653049dcfea9b3f889ff15edff513f673423
MOHD NEHAL KHAN <nehalk482@gmail.com>
2024-10-09 12:16:48 +0530

    Added links of all blogs ,change order from latest to oldest & animation

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -5,7 +5,7 @@ function BlogCard({blogname, blogdesc, img, link}) {
   return (
     
 
-<div className="max-w-xs rounded-md  bg-white overflow-hidden transform hover:translate-x-1 hover:-translate-y-1 hover:shadow-[-8px_8px_0px_#000] transition-all duration-300">
+<div className="max-w-xs rounded-md  bg-white overflow-hidden transform hover:translate-x-1 hover:-translate-y-1 hover:shadow-[-8px_8px_0px_#fb7185] transition-all duration-300">
   <img className="w-full rounded-md" src={img} alt="Sunset in the mountains" />
   <div className="px-6 py-4">
     <div className="font-bold text-xl mb-2">{blogname}</div>
@@ -16,7 +16,7 @@ function BlogCard({blogname, blogdesc, img, link}) {
               href={link}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-blue-500 hover:no-underline"
+              className="text-slate-400 text-lg font-semibold font-[kanit] hover:no-underline"
             >
               Blog Link🚀
             </a>
