85e7fb3d72b3ae90bb6772395c9fea1e68c49d42
MOHD NEHAL KHAN <nehalk482@gmail.com>
2024-10-11 15:39:58 +0530

    Modified The project section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,15 +1,38 @@
 import ProjectCard from "./ProjectCard";
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import ScrollTrigger from "gsap/ScrollTrigger";
+import WeatherImg from "../assets/Weather-pic 2.svg";
+import TaskImg from "../assets/Task-page.svg";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function Project() {
+  useGSAP(() => {
+
+    gsap.from(".project-head", {
+      opacity: 0,
+      y: -400,
+      duration: 2,
+      scrollTrigger: {
+        trigger: ".project-head",
+        start: 'top +=300',
+        end: '+=350',
+        scrub: 2,
+      },
+    });
+  });
   return (
-    <div className="flex flex-col px-5 py-28 lg:px-10 xl:px-10  w-full ">
-<div className="flex flex-col items-center">
+    <div className="flex flex-col px-5 py-28 lg:px-10 xl:px-10  w-full bg-blue-300 overflow-hidden h-dvh ">
+<div className="project-head flex flex-col items-center">
             <p className="font-['Goldman'] text-2xl font-bold text-gray-800">See My</p>
             <p className="font-['Goldman'] text-5xl font-bold text-rose-400">Projects</p>
             </div>
             <div className="project-sec flex flex-wrap gap-5 py-10 justify-center">
-
+<ProjectCard ProjectName="Weather App"  ProjectImg={WeatherImg} GitHubLink="https://github.com/MOHDNEHALKHAN/Weather-App" LiveLink="https://weather-app-rmx8.onrender.com/"/>
+<ProjectCard ProjectName="Todolist App"  ProjectImg={TaskImg} GitHubLink="https://github.com/MOHDNEHALKHAN/Todolist-app" LiveLink="https://todolist-app-flame-three.vercel.app/"/>
             </div>
+
     </div>
   )
 }
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,22 +1,29 @@
 
-
-function ProjectCard() {
+// eslint-disable-next-line react/prop-types
+function ProjectCard({ProjectName, ProjectImg, GitHubLink, LiveLink}) {
   return (
     <div className="max-w-xs rounded-md  bg-white overflow-hidden transform hover:translate-x-1 hover:-translate-y-1 hover:shadow-[-8px_8px_0px_#fb7185] transition-all duration-300">
-  <img className="w-full rounded-md" src="" alt="Sunset in the mountains" />
+  <img className="object-contain rounded-md" src={ProjectImg} alt="Project Image" />
   <div className="px-6 py-4">
-    <div className="font-bold text-xl mb-2"></div>
-    <p className="text-gray-700 text-base">
-     
-    </p>
+    <div className="font-bold text-xl mb-2">{ProjectName}</div>
+    <div className="flex justify-between">
     <a
-              href=""
+              href={GitHubLink}
               target="_blank"
               rel="noopener noreferrer"
               className="text-slate-400 text-lg font-semibold font-[kanit] hover:no-underline"
             >
-                Project Link🚀
+                GitHub Link🚀
             </a>
+             <a
+              href={LiveLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-slate-400 text-lg font-semibold font-[kanit] hover:no-underline"
+            >
+                Live Project🚀
+            </a>
+            </div>
   </div>
 </div>
   )
